c6a4767a3f6bd44538115099836643264b492dc9
Omar Farah <farah.omar8787@gmail.com>
2020-04-20 23:38:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,15 +10,6 @@ end
 end
 
 def not_safe?(speed)
-  speed > 60 ? true : speed < 40 
+  speed > 60 ? true : speed < 40 ? true : false 
 
-def not_safe?(speed)
-  if speed > 60
-    return true
-  elsif speed < 40
-    return true
-  else
-    return false
-
-end
 end
